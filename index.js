89e322bbacb11c7ec11fbdb5b87e2e06e28415ae
pshivvy <shivnpatel27@gmail.com>
2019-05-18 15:50:37 -0400

    Added social-link images and created index.js to add some hover animation and also for future uses

---
+++ index.js
@@ -0,0 +1,20 @@
+function gitOver(){
+    document.getElementById('github').src = "images/hovergithubicon.png";
+}
+function gitOut(){
+    document.getElementById('github').src = "images/githubicon.png";
+}
+
+function redditOver(){
+    document.getElementById('reddit').src = "images/hoverredditicon.png";
+}
+function redditOut(){
+    document.getElementById('reddit').src = "images/redditicon.png";
+}
+
+function ytOver(){
+    document.getElementById('youtube').src = "images/hoveryoutubeicon.png";
+}
+function ytOut(){
+    document.getElementById('youtube').src = "images/youtubeicon.png";
+}
